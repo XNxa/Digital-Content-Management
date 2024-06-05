9f59d41f17f0f7e3c0ad812ef9a45806685cca0f
XNxa <xaviernaxara@yahoo.fr>
2024-06-05 17:53:57 +0100

    Fixed bug with name

--- src/main/java/com/dcm/backend/BackendController.java
+++ src/main/java/com/dcm/backend/BackendController.java
@@ -2,28 +2,27 @@ package com.dcm.backend;
 
 import com.dcm.backend.beans.File;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class BackendController {
 
     @Autowired
-    BackendRepository facade;
+    FileService fs;
 
-    @GetMapping("/files")
-    public Iterable<File> getAll() {
-        return facade.findAll();
-    }
 
-    @GetMapping("/new")
-    public void add(@RequestParam("name") String name) {
-        File f = new File();
-        f.setFilename(name);
-        facade.save(f);
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        if (fs.uploadFile(file)) {
+            return ResponseEntity.ok("File successfully uploaded");
+        } else {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
 }
---
+++ src/main/java/com/dcm/backend/MinioConfig.java
@@ -0,0 +1,16 @@
+package com.dcm.backend;
+
+import io.minio.MinioClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint("http://localhost:9000")
+                .credentials("admin", "password")
+                .build();
+    }
+}
