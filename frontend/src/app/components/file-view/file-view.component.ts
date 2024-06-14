12164247018e7e5aedf4190a164b66f9cb764031
Xavier Naxara <xaviernaxara@yahoo.fr>
2024-06-14 10:54:49 +0100

    Cleaned an comment file-view.ts

--- frontend/src/app/components/file-view/file-view.component.ts
+++ frontend/src/app/components/file-view/file-view.component.ts
@@ -31,20 +31,46 @@ import { Status } from '../../enums/status';
   styleUrl: './file-view.component.css'
 })
 export class FileViewComponent implements OnInit {
-  viewtitle = "Fichiers";
-  gridlayout = true;
-  isDialogOpen: boolean = false;
-  files: FileHeader[] = [];
-  currentPage: number = 1;
-  itemsPerPage: number = 16;
-  numberOfElements!: number;
-  keywords!: string[];
-  status = Status.getStringList();
 
-  filenameSearched: string = '';
-  keywordsSearched: string[] = [];
-  statusSearched: string[] = [];
+  /** The title of the file view. */
+  readonly viewtitle: string = "Fichiers";
+
+  /** The number of items to display per page. */
+  readonly itemsPerPage: number = 16;
   
+  /** Array of status strings. */
+  readonly status : string[] = Status.getStringList();
+
+  /** Flag indicating whether to display files in grid layout or list layout. */
+  gridlayout: boolean = true;
+
+  /** Flag indicating whether the upload dialog is open or not. */
+  isDialogOpen: boolean = false;
+
+  /** Array of file headers to display. */
+  files: FileHeader[] = [];
+
+  /** The current page number. */
+  currentPage: number = 1;
+
+  /** The total number of elements. */
+  numberOfElements!: number;
+
+  /** Array currently defined keywords. */
+  keywords!: string[];
+
+  /** The filename being searched. 
+   * Binded to the search field */
+  filenameSearched: string = '';
+
+  /** Array of keywords being searched. 
+   * Binded to the search field */
+  keywordsSearched: string[] = [];
+
+  /** Array of status strings being searched. 
+   * Binded to the search field */
+  statusSearched: string[] = [];
+
   constructor(private api: FileApiService, private snackbar: SnackbarService) {
     this.api.getNumberOfElement().subscribe(n => {
       this.numberOfElements = n;
@@ -53,35 +79,46 @@ export class FileViewComponent implements OnInit {
       this.keywords = keywords;
     });
   }
-  
+
   ngOnInit(): void {
     this.refreshFileList();
   }
-  
+
+  /**
+   * Event handler for page change.
+   * @param pageNumber The new page number.
+   */
   onPageChange(pageNumber: number): void {
     this.currentPage = pageNumber
     this.refreshFileList();
   }
-  
+
+  /** Event handler for list view button click. */
   onListClicked(): void {
     this.gridlayout = false;
   }
 
+  /** Event handler for grid view button click. */
   onGridClicked(): void {
     this.gridlayout = true;
   }
 
-  refreshFileList(): void {
+  /** Fetch file list from the server. Get files of current page only. */
+  private refreshFileList(): void {
+    // Get the files for the current page and search criteria
     this.api.getPages(this.currentPage - 1, this.itemsPerPage, this.filenameSearched, this.keywordsSearched, this.statusSearched.map(s => Status.fromString(s))).subscribe(files => {
       this.files = files;
       for (let file of this.files) {
         if (file.thumbnailName != null) {
+          // Get the thumbnail for each file
           this.api.getThumbnail(file.filename).subscribe(blob => {
             file.thumbnail = URL.createObjectURL(blob);
           });
         }
       }
     });
+
+    // Refresh the list of keywords
     this.api.getKeywords().subscribe(keywords => {
       this.keywords = keywords;
     });
