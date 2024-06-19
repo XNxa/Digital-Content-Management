e3eb7f06a5d4efe24455b95a4eb02a2964048cd6
Xavier Naxara <xaviernaxara@yahoo.fr>
2024-06-19 14:39:16 +0100

    Created zoom control

---
+++ frontend/src/app/shared/components/buttons/zoom-button/zoom-button.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-zoom-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './zoom-button.component.html',
+  styleUrl: './zoom-button.component.css'
+})
+export class ZoomButtonComponent {
+
+  zoom = 1;
+  @Output() onZoom : EventEmitter<number> = new EventEmitter<number>();
+
+  restoreZoom(): void {
+    this.zoom = 1;
+    this.onZoom.emit(this.zoom);
+  }
+
+  zoomIn(): void {
+    if (this.zoom < 2) {
+      this.zoom += 0.1;
+    }
+    this.onZoom.emit(this.zoom);
+  }
+
+  zoomOut(): void {
+    if (this.zoom > 0.1) {
+      this.zoom -= 0.1;
+    }
+    this.onZoom.emit(this.zoom);
+  }
+
+}
