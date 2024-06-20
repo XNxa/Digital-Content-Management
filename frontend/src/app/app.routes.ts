6276c1b17df776900886b900e52562875b2dbe44
Xavier Naxara <xaviernaxara@yahoo.fr>
2024-06-20 09:51:39 +0100

    Cleanup

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { InputComponent } from './shared/components/form/input/input.component';
 
 export const routes: Routes = [
     {path: '**', component: HomeComponent}
