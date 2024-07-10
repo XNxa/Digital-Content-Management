0bd558ac0faa2c48f5cc33d869f34fa047a85840
Xavier Naxara <xaviernaxara@yahoo.fr>
2024-07-10 15:45:13 +0100

    Updated directive to allow multiple permissions

--- frontend/src/app/shared/directives/permission.directive.ts
+++ frontend/src/app/shared/directives/permission.directive.ts
@@ -9,8 +9,8 @@ export class PermissionDirective {
 
   constructor(private keycloak: KeycloakService, private elementRef: ElementRef) { }
   
-  @Input() set permission(permission: string) {
-    if (!this.keycloak.isUserInRole(permission)) {
+  @Input() set permission(permission: string[]) {
+    if (!permission.some(p => this.keycloak.isUserInRole(p))){
       this.elementRef.nativeElement.remove();
       this.elementRef.nativeElement.style.display = 'none'; // When can't remove, hide it
     }
